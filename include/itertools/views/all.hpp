456fad491c73bca01ef7b54032c18b135cae8f0b
Mike Babb <mike7400@gmail.com>
2020-12-09 21:39:39 -0500

    Added all header.

---
+++ include/itertools/views/all.hpp
@@ -0,0 +1,11 @@
+#include "block.hpp"
+#include "concat.hpp"
+#include "enumerate.hpp"
+#include "filter.hpp"
+#include "range.hpp"
+#include "slice.hpp"
+#include "stride.hpp"
+#include "transform.hpp"
+#include "zip.hpp"
+
+#pragma once
